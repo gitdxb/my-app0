aa2f41e3e5ac82e8199f2ed5183472e1b74f237c
Tam Tran <35073844+gitdxb@users.noreply.github.com>
2023-02-18 20:38:40 +0700

    new CSS update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import reactLogo from "../images/react-logo.png"
 
 export default function Navbar() {
   return (
     <nav>
-        <img  className='nav--icon' src="..\images\react-logo.png" alt="ascasc" />
+        <img  className='nav--icon' src={reactLogo} alt="ascasc" />
         <h3 className='nav--logo_text'>ReactFacts</h3>
         <h4 className='nav--title'>React Course - Project 1</h4>
     </nav>
